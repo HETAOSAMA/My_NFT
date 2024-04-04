241e1049e8c965a4d96bfdcdb975e3136a626c15
bv47 <1131573373@qq.com>
2024-04-04 12:05:25 +0800

    Update My_Nft.sol

--- contracts/My_Nft.sol
+++ contracts/My_Nft.sol
@@ -10,28 +10,39 @@ contract MyNft is ERC721URIStorage {
     //权限控制
     mapping (address =>(mapping string => bool)) public hasRole;
     uint256 public MAX_APES = 10000; // 总量
-    uint256 private _nextTokenId = 1; // 下一个NFT的ID, 从1开始,因为省gas
+    uint256 private _nextTokenId = 1; // 下一个NFT的ID, 从1开始,0是保留的
     //mint事件
     event Mint(address indexed to, uint256 amount);
     //权限事件
     event RoleAdded(address indexed to, string role, bool isApproved);
+    //集合元数据更新事件
+    event ContractURIUpdated(string uri);
+
     //ERC721指定NFT的名字和简称 
     constructor() ERC721("MyNft", "MNFT"){
         owner = msg.sender;
     }
 
-    function getEthToUsd() public payable {
+    //要做盲盒类型的NFT，可以使用预言机来获取一些随机数字，然后根据这个数字来生成NFT或者调用外部接口
+    function mintNFT() public payable {
         require(msg.velue.convert() >= 100, "Not enough ETH");
         
         require(tokenId < MAX_APES, "All MyNft have been minted");
         _mint(msg.sender, tokenId);
+        //存入的是NFT的元数据，可以是ipfs的CID，也可以是http的URL   
         _setTokenURI(tokenId, "https://ipfs.io/ipfs/QmZ ...");
         uint256 tokenId = _nextTokenId++;
         //mint 1个NFT
         emit Mint(msg.sender, 1);
     }
 
-    //添加权限
+    //返回集合元数据，OpenSea等市场会调用这个方法(包含NFT的名字，描述，图片或者说是logo等)，创作者收费等需要去平台登记
+    function contractURI() public view returns (string memory) {
+        return "ipfs://QmTNgv3jx2HHfBjQX9RnKtxj2xv2xQDtbVXoRi5rJ3a46e";
+        // or e.g. https://external-link-url.com/my-contract-metadata.json
+    }
+
+    //添加权(通过函数名来控制权限)
     function addRole(address _address, string memory _role, bool isApproved) public {
         require(msg.sender == owner, "You are not the owner");
         hasRole[_address][_role] = isApproved;
