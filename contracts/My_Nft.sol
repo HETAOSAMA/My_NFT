e346db2c276a4c801a85ef25d81c2f8ed11863ef
bv47 <1131573373@qq.com>
2024-04-05 11:40:17 +0800

    问题修复，现可正常compile

--- contracts/My_Nft.sol
+++ contracts/My_Nft.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.20;
 
 import "./lib/utils/EthToUsd.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNft is ERC721URIStorage {
     using EthToUsd for uint256;
     address owner;
     //权限控制
-    mapping (address =>(mapping string => bool)) public hasRole;
+    mapping(address => mapping(string => bool)) public hasRole;
     uint256 public MAX_APES = 10000; // 总量
     uint256 private _nextTokenId = 1; // 下一个NFT的ID, 从1开始,0是保留的
     //mint事件
@@ -25,19 +26,19 @@ contract MyNft is ERC721URIStorage {
 
     //要做盲盒类型的NFT，可以使用预言机来获取一些随机数字，然后根据这个数字来生成NFT或者调用外部接口
     function mintNFT() public payable {
-        require(msg.velue.convert() >= 100, "Not enough ETH");
-        
-        require(tokenId < MAX_APES, "All MyNft have been minted");
-        _mint(msg.sender, tokenId);
+        require(msg.value.convert() >= 100, "Not enough ETH");
+   
+        require(_nextTokenId < MAX_APES, "All MyNft have been minted");
+        _mint(msg.sender, _nextTokenId);
         //存入的是NFT的元数据，可以是ipfs的CID，也可以是http的URL   
-        _setTokenURI(tokenId, "https://ipfs.io/ipfs/QmZ ...");
-        uint256 tokenId = _nextTokenId++;
+        _setTokenURI(_nextTokenId, "https://ipfs.io/ipfs/QmZ ...");
+        _nextTokenId = _nextTokenId++;
         //mint 1个NFT
         emit Mint(msg.sender, 1);
     }
 
     //返回集合元数据，OpenSea等市场会调用这个方法(包含NFT的名字，描述，图片或者说是logo等)，创作者收费等需要去平台登记
-    function contractURI() public view returns (string memory) {
+    function contractURI() public pure returns (string memory) {
         return "ipfs://QmTNgv3jx2HHfBjQX9RnKtxj2xv2xQDtbVXoRi5rJ3a46e";
         // or e.g. https://external-link-url.com/my-contract-metadata.json
     }
@@ -51,7 +52,7 @@ contract MyNft is ERC721URIStorage {
     }   
 
     //将合约中的所有ETH转给调用者，调用者必须拥有权限
-    function withdraw public{
+    function withdraw() public{
         require(hasRole[msg.sender]["withdraw"], "You are not the owner");
         //transfer gass上限为2300，会主动抛出异常
         // payable(msg.sender).transfer(address(this).balance);
--- contracts/lib/utils/EthToUsd.sol
+++ contracts/lib/utils/EthToUsd.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+
 library EthToUsd {
-    private constant ETH_USD_PRICE_FEED = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+    address private constant ETH_USD_PRICE_FEED = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
 
     function getLatestPrice() internal view returns (uint256) {
         AggregatorV3Interface priceFeedInstance = AggregatorV3Interface(ETH_USD_PRICE_FEED);
